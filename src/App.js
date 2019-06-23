4f2ded1495baa793ebd5dacb62d90b825363f656
AhmedSoror <ahmedsoror438@gmail.com>
2019-06-24 00:07:27 +0200

    create todo component

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ import TodosList from './components/todos-list.component';
 import EditTodo from './components/edit-todo.component';
 import CreateTodo from './components/create-todo.component';
 
-import logo from "./logo.png";
+import logo from "./githubLogo2.svg";
 
 class App extends Component {
   render(){
     return (
-      <Router>
-      <div className="container">
-        
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-            <img src={logo} width="35" height="35" alt="CodingTheSmartWay.com" />
+      <Router> 
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="https://github.com/AhmedSoror/ToDoList" target="_blank">
+            <img src={logo} width="35" height="35" alt="Github source code" />
           </a>
           <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
           <div className="collpase nav-collapse">
@@ -26,15 +24,15 @@ class App extends Component {
               </li>
               <li className="navbar-item">
                 <Link to="/create" className="nav-link">Create Todo</Link>
-              </li>
+              </li>            
             </ul>
           </div>
         </nav>
-
-        <Route path="/" exact component={TodosList} />
-        <Route path="/edit/:id" component={EditTodo} />
-        <Route path="/create" component={CreateTodo} />
-      </div>
+        <div className="container">
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
     </Router>
     );
   }
