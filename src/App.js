6f38e47b931bb09e729e9e3963377b03bb827c26
AhmedSoror <ahmedsoror438@gmail.com>
2019-06-23 22:07:59 +0200

    create react

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    
+    <div className="container">
+      <h2>MERN-Stack Todo App</h2>
+    </div>
   );
 }
 
